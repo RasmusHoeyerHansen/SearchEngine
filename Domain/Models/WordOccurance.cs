cc3fef84131d2952dad0f23536eeaf7e2e435ef8
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-11-21 11:45:57 +0100

    Domain models

--- Domain/Models/WordOccurance.cs
+++ Domain/Models/WordOccurance.cs
@@ -1,9 +1,30 @@
-﻿namespace Domain.Models
-{
-    public class WordOccurance : 
+﻿using System.Collections;
+using System.Collections.Generic;
 
+namespace Domain.Models
+{
+    public class WordOccurance
     {
-    public string Literal { get; set; }
-    public int Occurances { get; set; }
+        private Article Article { get; set; }
+        public int Occurances { get; set; }
+        public Word Word { get; set; }
     }
+
+    public class Word
+    {
+        public string Literal { get; set; }
+    }
+
+    public class Article
+    {
+        public List<WordOccurance> WordOccurances;
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public List<Article> WrittenArticles { get; set; }
+    }
+    
+    
 }
