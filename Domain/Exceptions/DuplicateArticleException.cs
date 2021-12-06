afc9416d6a2dce50814b60c0fabaa0530ff1b9f4
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-12-06 21:00:38 +0100

    Created exception

---
+++ Domain/Exceptions/DuplicateArticleException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Domain_models.Entities;
+
+namespace Domain_models.Exceptions
+{
+    public class DuplicateArticleException : Exception
+    {
+        public DuplicateArticleException(string message = "Article is already in database")
+        :base(message)
+        {
+            
+        }
+        
+        public DuplicateArticleException(Guid id) : this(id.ToString() + " is already in database")
+        {
+            
+        }
+    }
+}
