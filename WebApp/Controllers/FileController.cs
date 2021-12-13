78ea5c11f6da0aeb5e5544dd2dbdf07ac0fc5441
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-12-13 20:06:38 +0100

    react project

---
+++ WebApp/Controllers/FileController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PreProcessing.Common.Exceptions;
+using PreProcessing.Common.Services;
+
+namespace Website.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly IKnowledgeFromTextService<Stream> ExtractionFromTextService;
+
+        public FileController(IKnowledgeFromTextService<Stream> extractionFromTextService)
+        {
+            ExtractionFromTextService = extractionFromTextService;
+        }
+
+        [HttpPost]
+        [Route("/[controller]/Extract")]
+        public IActionResult PostFile(IFormFile file)
+        {
+            IActionResult result = Ok();
+            try
+            {
+                ExtractionFromTextService.ExtractKnowledge(file.OpenReadStream());
+            }
+            catch (PdfParsingException e)
+            {
+                result = BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                result = BadRequest("Corrupted File");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Website.Controllers;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        private readonly IConfiguration Config;
+
+        public Startup(IConfiguration config)
+        {
+            this.Config = config;
+        }
+    
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Infrastructure.DependencyInjection.AddInfrastructure(services);
+            PreProcessing.DependencyInjection.AddPreProcessing(services);
+            KnowledgeExtraction.DependencyInjection.AddKnowledgeExtraction(services);
+            
+            services.AddControllersWithViews();
+            services.AddSingleton(new FileController(null));
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "My API", Version = "v1"}); });
+            // In production, the React files will be served from this directory
+        }
+    
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller}/{action=Index}/{id?}");
+            });
+
+
+            SetupSwagger(app);
+        }
+
+        private static void SetupSwagger(IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+        }
+    }
+}
